d3ff8af482e628f93292aed4d02afe3f5972eb28
Anton Khorev <tony29@yandex.ru>
2021-05-19 17:33:41 +0300

    basic cursorDown functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,20 +8,49 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-fake-virtual-space" is now active!');
+	console.log('Congratulations, your extension "fake-virtual-space" is now active!')
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-fake-virtual-space.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable=vscode.commands.registerCommand('fakeVirtualSpace.cursorDown',cursorDown)
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fake-virtual-space!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+async function cursorDown() {
+	vscode.window.showInformationMessage('cursorDown')
+	const moveCommand='cursorDown'
+	const editor=vscode.window.activeTextEditor!
+	const positionBefore=editor.selection.start
+	await vscode.commands.executeCommand(moveCommand)
+	const positionAfter=editor.selection.start
+	const insertion=getVerticalMoveInsertion(
+		Number(editor.options.tabSize),
+		positionBefore.character,
+		editor.document.lineAt(positionBefore).text,
+		positionAfter.character,
+		editor.document.lineAt(positionAfter).text
+	)
+	if (insertion!=null) {
+		editor.edit(editBuilder=>{
+			editBuilder.insert(positionAfter,insertion)
+		})
+	}
+}
+
+export function getVerticalMoveInsertion(
+	tabSize: number,
+	characterBefore: number,
+	textBefore: string,
+	characterAfter: number,
+	textAfter: string
+): string|null {
+	if (characterAfter<textAfter.length) return null
+	const diff=characterBefore-characterAfter
+	if (diff<=0) return null
+	return ' '.repeat(diff)
+}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,28 @@
-import * as assert from 'assert';
+import {strict as assert} from 'assert'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as vscode from 'vscode'
+import * as myExtension from '../../extension'
 
-suite('Extension Test Suite', () => {
+suite("Extension Test Suite",()=>{
 	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
-});
+	suite("getVerticalMoveInsertion",()=>{
+		test("returns null when moved to a position not at eol",()=>{
+			const result=myExtension.getVerticalMoveInsertion(8,
+				5,"was h!ere",
+				7,"came so!mewhere for some reason"
+			)
+			assert.equal(result,null)
+		})
+		test("returns required spaces on unindented strings",()=>{
+			const result=myExtension.getVerticalMoveInsertion(8,
+				6,"was he!re",
+				1,"!"
+			)
+			assert.equal(result,
+				   "     "
+			)
+		})
+	})
+})
