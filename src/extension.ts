fcabbbb120bb4adc98be26216c8e2374ae322c92
Anton Khorev <tony29@yandex.ru>
2021-05-19 17:44:51 +0300

    basic cursorUp functionality

--- src/extension.ts
+++ src/extension.ts
@@ -5,25 +5,24 @@ import * as vscode from 'vscode'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fake-virtual-space" is now active!')
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable=vscode.commands.registerCommand('fakeVirtualSpace.cursorDown',cursorDown)
-
-	context.subscriptions.push(disposable)
+	context.subscriptions.push(
+		vscode.commands.registerCommand('fakeVirtualSpace.cursorUp',cursorUp),
+		vscode.commands.registerCommand('fakeVirtualSpace.cursorDown',cursorDown)
+	)
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-async function cursorDown() {
-	vscode.window.showInformationMessage('cursorDown')
-	const moveCommand='cursorDown'
+function cursorUp() {
+	cursorVerticalMove('cursorUp')
+}
+
+function cursorDown() {
+	cursorVerticalMove('cursorDown')
+}
+
+async function cursorVerticalMove(moveCommand:string) {
 	const editor=vscode.window.activeTextEditor!
 	const positionBefore=editor.selection.start
 	await vscode.commands.executeCommand(moveCommand)
